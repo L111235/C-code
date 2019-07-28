49cc492e3ac5049af3a098d35edcf4d683b14ea6
L111235 <liangzhibin676@outlook.com>
2019-07-28 17:17:04 +0800

    https://github.com/L111235/C-code.git

---
+++ Project1/源.cpp
@@ -0,0 +1,11 @@
+// myfirst.cpp--display a message
+
+#include<iostream>                              //a preprocessor directive
+int main()                                      //function header
+{                                              //start of function body
+	using namespace std;                       //make definitions visible
+	printf("Come up and C++ me some time.\n");   //message
+												   //statr a new line
+	printf("You won`t regert it!\n");         //more output
+	return 0;                                  //terminate main()
+}                                              //end of function body
